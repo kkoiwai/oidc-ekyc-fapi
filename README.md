9e4972ac3d869d4b6bc5d1d2c83b08055463d01b
Kosuke Koiwai <kkoiwai@gmail.com>
2022-03-27 13:25:46 +0900

    Initial commit: fork from OIDC_for_SPA

---
+++ README.md
@@ -0,0 +1,26 @@
+ # OIDC eKYC RP on FAPI
+
+## About
+
+very simple implementation of `verified_claims` retrieval using OIDC-eKYC and FAPI (detail profile not determined)
+
+## Try it
+
+
+
+## How to use this
+
+
+```
+$ npm install
+$ node app.js
+```
+1. open browser and goto http://localhost:3030
+
+## Author
+
+Kosuke Koiwai (https://github.com/kkoiwai)
+
+## Details 
+
+TBA
---
+++ sqlite_init.js
@@ -0,0 +1,13 @@
+// now you don't have to call this.
+// this is left for cleaning table
+
+const sqlite3 = require('sqlite3'),
+    db = new sqlite3.Database('oidc_db.sqlite');
+db.serialize();
+
+// Create session table
+db.run('DROP TABLE IF EXISTS session');
+db.run("CREATE TABLE session (session_id TEXT UNIQUE NOT NULL, pkce_code TEXT, subject_id TEXT, user_info TEXT, active INTEGER NOT NULL DEFAULT 1, created_at TEXT NOT NULL DEFAULT (DATETIME('now', 'localtime')))");
+
+db.close();
+
